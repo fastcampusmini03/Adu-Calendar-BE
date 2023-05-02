835e18ddfb30e0b7c68a9f986a20a67b567f52ef
taeheoki <42.4.taeheoki@gmail.com>
2023-05-02 15:56:10 +0900

    user model 완료

--- src/main/java/com/fastcampus03/calendarbe/dto/ResponseDTO.java
+++ src/main/java/com/fastcampus03/calendarbe/dto/ResponseDTO.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 public class ResponseDTO<T> {
     private Integer status; // 에러시에 의미 있음.
     private String msg; // 에러시에 의미 있음. ex) badRequest
-    private T data; // 에러시에는 구체적인 에러 내용 ex) username이 입력되지 않았습니다
+    private T data; // 에러시에는 구체적인 에러 내용 ex) email이 입력되지 않았습니다
 
     public ResponseDTO(){
         this.status = HttpStatus.OK.value();
